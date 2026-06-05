098545acfd53442b1d7ab1158dbf696c99fecd30
Harry <128583280+kaito-harry@users.noreply.github.com>
2026-06-05 17:25:33 +0800

    chore: move DataStreamService to standalone repo
    Now at /Users/kaito/Project/Actrium/DataStreamService
    Repo: https://github.com/kaito-harry/datastream-service

--- DataStreamService/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // actr gen already generated src/generated/ with prost types and actor code.
-    // No build-time code generation needed.
-    println!("cargo:rerun-if-changed=src/generated/");
-}
